f3b57659e032deca10c20766835b922b59eb428d
Miguel © <233833781+AlguemDev@users.noreply.github.com>
2026-01-15 02:06:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,3 @@ const observer = new IntersectionObserver(entries => {
 });
 
 elements.forEach(el => observer.observe(el))
-
-const elements = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  const windowHeight = window.innerHeight;
-
-  elements.forEach(el => {
-    const elementTop = el.getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("load", revealOnScroll);
